0712a605bf061039d18c6041fae2c9e7e81c63bf
Galina Sidarenka <galia.sidarenka@icloud.com>
2022-01-10 00:11:53 -0500

    cleanup

--- src/server/start.js
+++
@@ -1,9 +0,0 @@
-const app = require('./server');
-
-const port = 8080;
-
-const server = app.listen(port, listening);
-function listening() {
-    console.log(server);
-    console.log(`running on localhost: ${port}`);
-}
